2111ec2316d64b9dbbb942c7116cb0ea07814870
Stephan Reichholf <stephan@reichholf.net>
2026-05-28 16:12:29 +0200

    use relative path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   build: {
     chunkSizeWarningLimit: 800, // Silences warnings for larger cacheable third-party chunks
     rollupOptions: {
